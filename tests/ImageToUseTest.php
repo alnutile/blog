b445d9cf715ece67783f29a242b0782566a83e0a
alnutile <alfrednutile@gmail.com>
2017-08-04 19:01:17 -0400

    Rename so it actually is tested

---
+++ tests/ImageToUseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ImageToUseTest extends TestCase
+{
+
+    public function testSeeingIfExistOnAWS()
+    {
+        $image = new \App\ImageToUse();
+
+        $results = $image->exists("foo.png");
+
+        $this->assertEquals("/img/foo.png", $results);
+    }
+}
