4a96db02fa537566d32a7fcba891663552c8292f
Laravel Shift <shift@laravelshift.com>
2017-08-04 23:02:35 +0000

    Shift tests

--- tests/RSSTest.php
+++ tests/RSSTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class RSSTest extends TestCase
+class RSSTest extends BrowserKitTestCase
 {
     use DatabaseTransactions, DatabaseMigrations;
 
--- tests/TagsShowTest.php
+++ tests/TagsShowTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class TagsShowTest extends TestCase
+class TagsShowTest extends BrowserKitTestCase
 {
 
     use DatabaseMigrations;
