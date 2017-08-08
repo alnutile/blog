aa7fddd1494ff4d874936b1e813158434d497200
alnutile <alfrednutile@gmail.com>
2017-08-08 19:04:10 -0400

    Gotta move command into class testing and mocking in the command class is not working as hoped for

--- app/Console/Commands/CanaryBuildCommand.php
+++ app/Console/Commands/CanaryBuildCommand.php
@@ -3,9 +3,11 @@
 namespace App\Console\Commands;
 
 use GuzzleHttp\Client;
+use function GuzzleHttp\Psr7\parse_header;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class CanaryBuildCommand
@@ -21,6 +23,10 @@ class CanaryBuildCommand extends Command
 
     protected $travis_state = "";
 
+    protected $sleep = 5;
+
+    protected $tries = 1;
+
     /**
      * @var Client
      */
@@ -28,31 +34,45 @@ class CanaryBuildCommand extends Command
 
     private $account_and_repo = "alnutile/alsblog5";
     private $travis_job_id;
+    private $build_state;
 
     public function __construct(Client $client)
     {
-        parent::__construct();
-
         $this->client = $client;
+        parent::__construct();
     }
 
     public function handle()
     {
-        /** @var Response $results */
-        $results = $this->triggerGitHubBuild();
+        try {
+            /** @var Response $results */
+            $results = $this->triggerGitHubBuild();
 
-        if ($results->getStatusCode() == 204) {
-            $this->info(sprintf("Results %d so going to manually trigger the build", $results->getStatusCode()));
-            //Well nothing to merge so just do a build anyways
-            //  else it would have done it i on it's own by just merging above
-            $results = $this->triggerTravisBuild();
+            if ($results->getStatusCode() == 204) {
+                $this->info(
+                    sprintf(
+                        "Results %d so going to manually trigger the build",
+                        $results->getStatusCode()
+                    )
+                );
+                //Well nothing to merge so just do a build anyways
+                //  else it would have done it i on it's own by just merging above
+                $results = $this->triggerTravisBuild();
 
-            $this->beginTravisWatcher(json_decode($results->getBody(), true));
+                $this->beginTravisWatcher(json_decode($results->getBody(), true));
 
-            $this->info(sprintf("Results from Travis %d", $results->getStatusCode()));
+                $this->info(sprintf("Results from Travis %d", $results->getStatusCode()));
+            }
+        } catch (\Exception $e) {
+            $message = sprintf(
+                "Something went horribly wrong :( %s, %d, %s",
+                $e->getMessage(),
+                $e->getLine(),
+                $e->getFile()
+            );
+            Log::info($message);
+            $this->error($message);
         }
-
-        $this->info(sprintf("Results from GitHub %d", $results->getStatusCode()));
     }
 
     public function seeIfTravisJobDone($job_id = false)
@@ -115,7 +135,7 @@ class CanaryBuildCommand extends Command
      */
     public function triggerGitHubBuild()
     {
-        $this->client->post(sprintf("https://api.github.com/repos/%s/merges", $this->account_and_repo), [
+        return $this->client->post(sprintf("https://api.github.com/repos/%s/merges", $this->account_and_repo), [
             'headers' => [
                 'X-GitHub-Media-Type' => 'application/vnd.github.v3+json',
                 'Authorization' => 'token ' . env('GITHUB_TOKEN'),
@@ -140,7 +160,144 @@ class CanaryBuildCommand extends Command
 
     public function beginTravisWatcher($results)
     {
-        $this->travis_job_id = $results['request']['id'];
+        $id = (isset($results['request']['id'])) ? $results['request']['id'] : $results['id'];
+        $this->travis_job_id = $id;
+        $message = sprintf("Running for id %d try %d", $id, $this->tries);
+        Log::info($message);
+        $this->info($message);
+        $this->seeIfHaveBuildInfoYet();
+    }
+
+    protected function seeIfHaveBuildInfoYet()
+    {
+
+        $response = $this->client->get(
+            sprintf(
+                "https://api.travis-ci.org/repo/%s/request/%d",
+                urlencode($this->account_and_repo),
+                $this->travis_job_id
+            ),
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Travis-API-Version' => '3',
+                    'Authorization' => 'token ' . env('TRAVIS_TOKEN'),
+                ]
+            ]
+        );
+
+        $results = json_decode($response->getBody(), true);
+
+        if (isset($results['builds']) && !empty($results['builds'])) {
+            foreach ($results['builds'] as $build) {
+                $this->reactToBuild($build);
+            }
+        } else {
+            $this->tryAgain();
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBuildState()
+    {
+        return $this->build_state;
+    }
+
+    /**
+     * @param mixed $build_state
+     */
+    public function setBuildState($build_state)
+    {
+        $this->build_state = $build_state;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTries()
+    {
+        return $this->tries;
+    }
+
+    /**
+     * @param int $tries
+     */
+    public function setTries($tries)
+    {
+        $this->tries = $tries;
+    }
+
+    /**
+     * @param int $sleep
+     */
+    public function setSleep($sleep)
+    {
+        $this->sleep = $sleep;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSleep()
+    {
+        sleep($this->sleep);
+    }
+
+    protected function buildErrored($build)
+    {
+        $this->build_state = 'errored';
+        $this->log("Errored");
+        return false;
+    }
+
+    protected function log($state)
+    {
+        Log::info(sprintf("%s on job %d", $state, $this->travis_job_id));
+    }
+
+    protected function buildCanceled($build)
+    {
+        $this->build_state = 'canceled';
+        $this->log("Canceled");
+        return false;
+    }
+
+    protected function buildStarted($build)
+    {
+        $this->build_state = 'started';
+        $this->log("Started");
+        $this->tryAgain();
+    }
+
+    protected function buildCreated($build)
+    {
+        $this->build_state = 'created';
+        $this->log("Created");
+        $this->tryAgain();
+    }
+
+    protected function buildBooting($build)
+    {
+        $this->build_state = 'booting';
+        $this->log("Booting");
+        $this->tryAgain();
+    }
+
+    protected function buildFailed($build)
+    {
+        $this->build_state = 'failed';
+        $this->log("Failed");
+        return false;
+    }
+
+    protected function buildPassed($build)
+    {
+        $this->build_state = 'passed';
+        $this->log("Passed!!!");
+        return false;
     }
 
     /**
@@ -157,5 +314,40 @@ class CanaryBuildCommand extends Command
     public function setTravisJobId($travis_job_id)
     {
         $this->travis_job_id = $travis_job_id;
+    }
+
+    protected function triggerMergeBackIntoMaster()
+    {
+        Log::info("Time to merge back into master");
+    }
+
+    protected function reactToBuild($build)
+    {
+        $method = 'build' . ucfirst($build['state']);
+        if (method_exists($this, $method)) {
+            Log::info("Method " . $method);
+            $this->{$method}($build);
+        } else {
+            Log::info("Method not found " . $method);
+            $this->tryAgain();
+        }
+    }
+
+    protected function tryAgain()
+    {
+        if ($this->tries > 100) {
+            return false;
+        }
+        $this->tries = $this->tries + 1;
+        $this->getSleep();
+        $message = sprintf(
+            "Try number %d for job %d state %s",
+            $this->tries,
+            $this->travis_job_id,
+            $this->build_state
+        );
+        $this->info($message);
+        Log::info($message);
+        $this->seeIfHaveBuildInfoYet();
     }
 }
--- tests/Feature/CanaryBuildTest.php
+++ tests/Feature/CanaryBuildTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Console\Commands\CanaryBuildCommand;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
+use Mockery\Mock;
 use TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -16,46 +18,135 @@ class CanaryBuildTest extends \TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->markTestIncomplete("Not mocked yet");
 
-    }
-    public function testTravisTriggerJob() {
-        /** @var CanaryBuildCommand $command */
-        $command = App::make(CanaryBuildCommand::class);
-
-        $response = $command->triggerTravisBuild();
-
-        //File::put(base_path('tests/fixtures/travis_response_from_starting_build.json'), $response->getBody());
+        $this->markTestIncomplete("Not mocked yet the command needs most of it pulled into a Service class so I can 
+        not deal with mocking a Command class");
     }
 
     public function testGetResultsOfDoneJobFromTravis() {
 
+        $client = \Mockery::mock(Client::class);
+
+        $body = File::get(base_path('tests/fixtures/travis_getting_results_using_a_request_id.json'));
+
+        $message = new \GuzzleHttp\Psr7\Response(
+            200, $headers =[], $body
+        );
+        $client->shouldReceive('get')->once()->andReturn($message);
         /** @var CanaryBuildCommand $command */
-        $command = App::make(CanaryBuildCommand::class);
+        $command = new CanaryBuildCommand($client);
+        //$command = App::make(CanaryBuildCommand::class);
 
         $response = $command->seeIfTravisJobDone(78708065);
 
-        //File::put(base_path('tests/fixtures/travis_response_from_done_job_with_success.json'), json_encode($response, 128));
-
         $command->beginTravisWatcher($response);
 
-
+        $this->assertNotNull($command->getTravisJobId());
     }
 
-    public function testGetStateOfStartedJobFromTravis()
+    public function testTravisTriggerJob() {
+
+        $client = \Mockery::mock(Client::class);
+
+        $client->shouldReceive('post')->once()->andReturn();
+
+        /** @var CanaryBuildCommand $command */
+        $command = new CanaryBuildCommand($client);
+        //$command = App::make(CanaryBuildCommand::class);
+        $command->triggerTravisBuild();
+        //File::put(base_path('tests/fixtures/travis_response_from_starting_build.json'), $response->getBody());
+    }
+
+
+
+    public function testGettingStateFromRequestBuild()
     {
         $response = json_decode(File::get(base_path('tests/fixtures/travis_response_from_start_job.json')), true);
 
-        /** @var CanaryBuildCommand $command */
-        $command = App::make(CanaryBuildCommand::class);
+        $body = File::get(base_path('tests/fixtures/travis_getting_results_using_a_request_id.json'));
 
+        /** @var CanaryBuildCommand $command */
+        //$command = App::make(CanaryBuildCommand::class);
+        $client = \Mockery::mock(Client::class);
+        $message = new \GuzzleHttp\Psr7\Response(
+            200, $headers =[], $body
+        );
+        $client->shouldReceive('get')->once()->andReturn($message);
+        $command = new CanaryBuildCommand($client);
+
+        /** @var CanaryBuildCommand $command */
         $command->beginTravisWatcher($response);
 
         $this->assertEquals(78707660, $command->getTravisJobId());
+
+        $this->assertEquals('passed', $command->getBuildState());
+    }
+
+    public function testingAllBuildStates()
+    {
+        $states = [
+            'started',
+            'passed',
+            'booting',
+            'created',
+            'canceled',
+            'failed',
+            'errored'
+        ];
+        foreach($states as $state) {
+            $response = json_decode(File::get(base_path('tests/fixtures/travis_response_from_start_job.json')), true);
+            $body = File::get(base_path('tests/fixtures/travis_getting_results_using_a_request_id.json'));
+            $body = str_replace('passed', $state, $body);
+            /** @var CanaryBuildCommand $command */
+            //$command = App::make(CanaryBuildCommand::class);
+            $client = \Mockery::mock(Client::class);
+            $message = new \GuzzleHttp\Psr7\Response(
+                200, $headers =[], $body
+            );
+            $client->shouldReceive('get')->once()->andReturn($message);
+            $command = new CanaryBuildCommand($client);
+            /** @var CanaryBuildCommand $command */
+            $command->beginTravisWatcher($response);
+            $this->assertEquals($state, $command->getBuildState());
+        }
+    }
+
+
+    public function testPassedAndTriesDone() {
+        $response = json_decode(File::get(base_path('tests/fixtures/travis_response_from_start_job.json')), true);
+        $body = File::get(base_path('tests/fixtures/travis_getting_results_using_a_request_id.json'));
+        $body = str_replace('passed', 'passed', $body);
+        /** @var CanaryBuildCommand $command */
+        //$command = App::make(CanaryBuildCommand::class);
+        $client = \Mockery::mock(Client::class);
+        $message = new \GuzzleHttp\Psr7\Response(
+            200, $headers =[], $body
+        );
+        $client->shouldReceive('get')->once()->andReturn($message);
+        $mocked = \Mockery::mock('\Symfony\Component\Console\Command\Command');
+        App::instance('\Symfony\Component\Console\Command\Command', $mocked);
+        $command = new CanaryBuildCommand($client);
+        /** @var CanaryBuildCommand $command */
+        $command->beginTravisWatcher($response);
+
+    }
+
+    protected function testMethodNotFoundFirstTime() {
+
+    }
+
+    protected function testIfNotDoneShouldTryForFiveTries() {
+
     }
 
     public function testGettingResponseFromTravisAndReactingToNotDone()
     {
+        //Should call to travis
+        //get not done the 1st time
+        //wait x seconds
+        //get done the next time
+        //close out
+
         //$response = File::get(base_path('tests/fixtures/travis_not_done.json'));
 
         /** @var CanaryBuildCommand $command */
@@ -65,5 +156,15 @@ class CanaryBuildTest extends \TestCase
 
         $this->assertEquals('finished', $command->getTravisState());
 
+    }
+
+    public function testOnlyWaitsForTenCycles() {
+
+    }
+
+    public function testingStatesFromTravis() {
+        /** @var CanaryBuildCommand $command */
+        $command = App::make(CanaryBuildCommand::class);
+        $command->seeIfTravisJobDone(78708065);
     }
 }
