115822764b0908f55d0af6194f289d5a3a150e70
alnutile <alfrednutile@gmail.com>
2017-08-19 12:33:54 -0400

    Ready to deploy

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -1,69 +1,71 @@
 <header>
-        <div class="navbar navbar-inverse">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
+<div class="container">
+    <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
 
-                    <a class="brand" href="/">AlfredNutile.info</a>
+                <a class="brand" href="/">AlfredNutile.info</a>
 
-                    <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li>
-                                <a href='/'>Home</a>
-                            </li>
-                            <li>
-                                <a href='/about'>About</a>
-                            </li>
-                            <li>
-                                <a href="/portfolio">Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="/posts">Blog &amp; Things</a>
-                            </li>
-                            <li>
-                                <a href="/products">Products</a>
-                            </li>
-                            <li>
-                                <a href="/learnings">Resources</a>
-                            </li>
-                            <li>
-                                <a href="/talks">Talks</a>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <!-- Authentication Links -->
-                            @if (Auth::guest())
-                                <li><a href="{{ route('login') }}">Login</a></li>
-                            @else
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                        Profile <span class="caret"></span>
-                                    </a>
+                <div class="nav-collapse collapse">
+                    <ul class="nav">
+                        <li>
+                            <a href='/'>Home</a>
+                        </li>
+                        <li>
+                            <a href='/about'>About</a>
+                        </li>
+                        <li>
+                            <a href="/portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="/posts">Blog &amp; Things</a>
+                        </li>
+                        <li>
+                            <a href="/products">Products</a>
+                        </li>
+                        <li>
+                            <a href="/learnings">Resources</a>
+                        </li>
+                        <li>
+                            <a href="/talks">Talks</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    Profile <span class="caret"></span>
+                                </a>
 
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li><a href="/projects/create">Create Project</a></li>
-                                        <li><a href="/posts/create">Create Post</a></li>
-                                        <li>
-                                            <a href="{{ route('logout') }}"
-                                               onclick="event.preventDefault();
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="/projects/create">Create Project</a></li>
+                                    <li><a href="/posts/create">Create Post</a></li>
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                                Logout
-                                            </a>
+                                            Logout
+                                        </a>
 
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                {{ csrf_field() }}
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 </header>
